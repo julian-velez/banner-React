c20198818dbe043181cd508be6bbe27e32b84bab
julian <juliansolo7189@gmail.com>
2024-05-24 20:14:37 -0500

    banner React

---
+++ ofer/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <h2>¡Oferta especial!</h2>
+      <p>Aprovecha nuestro descuento del 20% en todos los productos.</p>
+    </div>
+  );
+};
+
+export default Banner;
